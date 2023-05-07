1300517e76eb489d0a6251f65e0ab6fc51bc237e
petya0927 <unyipeti@gmail.com>
2023-05-07 16:05:00 +0200

    fix

--- components/ResponseElement.tsx
+++ components/ResponseElement.tsx
@@ -1,6 +1,7 @@
 import { ResponseT } from "@/utils/types";
 
 export default function ResponseElement({ response }: { response: ResponseT }) {
+  console.log(response.fields.media);
   return (
     <div className="rounded-2xl border-primary border-2 overflow-hidden w-full">
       <div className="bg-primary px-9 py-6">
@@ -8,7 +9,7 @@ export default function ResponseElement({ response }: { response: ResponseT }) {
           {response.fields.title}
         </h2>
       </div>
-      <div className="px-10 py-7 flex flex-col lg:flex-row items-start gap-4">
+      <div className="px-5 md:px-10 py-7 flex flex-col lg:flex-row items-start gap-4">
         <div className="w-full">
           {response.fields.description.content.map(
             (content: any, index: number) => (
@@ -19,7 +20,7 @@ export default function ResponseElement({ response }: { response: ResponseT }) {
             )
           )}
         </div>
-        {response.fields.media && (
+        {response.fields.media.fields && (
           <img
             src={`https:${response.fields.media.fields.file.url}`}
             className="w-72 ml-auto lg:ml-0"
