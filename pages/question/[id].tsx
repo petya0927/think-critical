eea8513e4394ff0a03c596cc78f00533495167fd
petya0927 <unyipeti@gmail.com>
2023-05-08 23:22:33 +0200

    fixed title max width

--- pages/question/[id].tsx
+++ pages/question/[id].tsx
@@ -21,7 +21,7 @@ export default function Question({ questionData }: { questionData: Question }) {
           <Menu isMenuOpen={isMenuOpen} isMainPage={false} />
           <PageHeader isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <div className="flex flex-col gap-16 items-center mt-10 sm:mt-16 px-7 md:px-32 xl:px-80 2xl:px-[500px] relative max-w-[2000px] m-auto">
-            <h1 className="font-semibold text-4xl md:text-6xl text-center">
+            <h1 className="font-semibold text-4xl md:text-6xl text-center max-w-[790px]">
               {questionData.fields.title}
             </h1>
             <Link href="/" className="xl:absolute xl:top-44 xl:left-40 mr-auto">
