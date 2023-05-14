37e948d9590aaace068dd6e3982f9c895ae687ba
petya0927 <unyipeti@gmail.com>
2023-05-14 20:11:59 +0200

    help site card fixes

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,16 +10,16 @@ export default function Card({
   buttonText: string;
 }) {
   return (
-    <div className="bg-primary rounded-[20px] px-10 py-9 w-full max-h-min flex flex-col gap-8">
+    <div className="bg-primary rounded-[20px] px-10 py-9 w-full max-h-min flex flex-col items-center gap-8">
       <h3 className="font-semibold text-2xl text-center">{title}</h3>
       <img
         src={image}
-        className="w-fit m-auto
+        className="w-fit mt-auto
       "
       />
       <Link
         href="/"
-        className="font-semibold text-2xl bg-white rounded-xl uppercase tracking-wide text-center w-fit md:w-full px-20 md:px-4 py-2 m-auto"
+        className="font-semibold text-2xl bg-white rounded-xl uppercase tracking-wide text-center w-full sm:w-fit lg:w-full sm:px-16 lg:px-0 py-2"
       >
         {buttonText}
       </Link>
