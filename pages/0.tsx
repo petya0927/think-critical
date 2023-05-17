6271f0f341b8ec83febaa28a003187a4437f77c6
petya0927 <unyipeti@gmail.com>
2023-05-17 23:00:10 +0200

    is this commit even real?

---
+++ pages/0.tsx
@@ -0,0 +1,15 @@
+export default function Zero() {
+  return (
+    <div className="h-screen flex flex-col">
+      <p>Is anything even real? Or is it just</p>
+      <img
+        src="https://qph.cf2.quoracdn.net/main-qimg-40cb739f1ec36fd67e62ac4fc23707ca"
+        alt=""
+        width={200}
+        height={200}
+        className="m-auto"
+      />
+      <p className="ml-auto">a lie?</p>
+    </div>
+  );
+}
