1a1ba5b59b33b3a95dd9c6c23463e2c2f9e450e9
petya0927 <unyipeti@gmail.com>
2023-05-18 00:25:35 +0200

    fix

--- pages/resthere.tsx
+++ pages/resthere.tsx
@@ -1,7 +1,7 @@
 export default function Resthere() {
   return (
-    <div className="h-screen bg-darkGray flex flex-col">
-      <p className="text-white m-auto text-center">
+    <div className="h-screen bg-darkGray flex flex-col items-center justify-center gap-16">
+      <p className="text-white text-center">
         If you&apos;ve made it this far, tired adventurer, rest up.
         <br />
         Don&apos;t overwork yourself and always remember not to take life too
@@ -9,7 +9,7 @@ export default function Resthere() {
       </p>
       <img
         src="https://i.kym-cdn.com/entries/icons/original/000/039/993/resthere.jpg"
-        className="w-2/3 m-auto"
+        className="w-full md:w-2/3"
       />
     </div>
   );
